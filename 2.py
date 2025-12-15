2ba01c9767b2ef30bf2bb86509445d719d5edffd
24331a05h6-sudhi <24331a05h6@mvgrce.edu.in>
2025-12-15 20:15:01 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,12 @@
+
+expr1 = 5 + 3 * 2
+print("Expression 1 (5 + 3 * 2):", expr1)
+
+expr2 = 2 * 3 ** 2
+print("Expression 2 (2 * 3 ** 2):", expr2)
+
+expr3 = 2 ** 3 ** 2
+print("Expression 3 (2 ** 3 ** 2):", expr3)
+
+expr4 = (2 ** 3) ** 2
+print("Expression 4 ((2 ** 3) ** 2):", expr4)
